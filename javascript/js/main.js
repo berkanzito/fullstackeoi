9438d23dd4de161b141db456471bc4f779d50ed3
Danilo Ulloa Pérez <danilouperez@gmail.com>
2018-10-08 22:54:59 +0100

    hotfix

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -1,23 +1,40 @@
 var attack = 1;
+var defense = 2;
+var atributo = 7;
 var dice = 20;
 
 function attackDice() {
   var numero = Math.floor((Math.random() * dice) + 1 + attack);
   document.getElementById('gimme');
   gimme.innerHTML = numero;
-
-  localStorage.setItem('attack', numero);
-  //var name = localStorage.getItem('result')
+  
+  ataques.push(numero);
+  document.getElementById("ataques").innerHTML = ataques;
 }
+var ataques = [];
+document.getElementById('ataques').innerHTML = ataques;
+localStorage.setObj('ataques', JSON.stringify(ataques));
 
 function defenseDice() {
   var numero = Math.floor((Math.random() * dice) + 1);
-document.getElementById('gimmed');
-gimmed.innerHTML = numero;
+  document.getElementById('gimmed');
+  
+  localStorage.setItem('defense', JSON.stringify(numero));
+  defensas.push(numero);
+  document.getElementById("defensas").innerHTML = defensas;
+  gimmed.innerHTML = numero;
 }
+var defensas = [];
+document.getElementById('defensas').innerHTML = defensas;
 
 function atributeDice() {
   var numero = Math.floor((Math.random() * dice) + 1);
-document.getElementById('gimmea');
-gimmea.innerHTML = numero;
+  document.getElementById('gimmea');
+  
+  localStorage.setItem('atributos', JSON.stringify(numero));
+  atributos.push(numero);
+  document.getElementById("atributos").innerHTML = atributos;
+  gimmea.innerHTML = numero;
 }
+var atributos = [];
+document.getElementById('atributos').innerHTML = atributos;
