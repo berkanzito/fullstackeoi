0e48df480db3b2a91e505df9a437b52d5980cccc
Danilo Ulloa <danilouperez@gmail.com>
2018-10-08 20:43:00 +0100

    hotfix

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -1,5 +1,23 @@
-function lanzarDado() {
-  var numero = Math.floor((Math.random() * 20) + 1);
-document.getElementById('gimme');
-gimme.innerHTML = numero;
+var attack = 1;
+var dice = 20;
+
+function attackDice() {
+  var numero = Math.floor((Math.random() * dice) + 1 + attack);
+  document.getElementById('gimme');
+  gimme.innerHTML = numero;
+
+  localStorage.setItem('attack', numero);
+  //var name = localStorage.getItem('result')
+}
+
+function defenseDice() {
+  var numero = Math.floor((Math.random() * dice) + 1);
+document.getElementById('gimmed');
+gimmed.innerHTML = numero;
+}
+
+function atributeDice() {
+  var numero = Math.floor((Math.random() * dice) + 1);
+document.getElementById('gimmea');
+gimmea.innerHTML = numero;
 }
