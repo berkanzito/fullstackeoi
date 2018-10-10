56e3a91c07de5829dff198c6057139e5461eeb16
Danilo Ulloa <danilouperez@gmail.com>
2018-10-10 19:00:53 +0100

    hotfix

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -24,20 +24,20 @@ var messages = [
 
 var showFace = function(d) {
   var face = Math.floor((Math.random() * dice) + 1);
-  if (d = 1) {
+  if (d == 1) {
     document.getElementById('gimme');
     gimme.innerHTML = face;
     
     ataques.push(face + attack);
     document.getElementById("ataques").innerHTML = ataques;
-  } else if (d = 2) {
+  } else if (d == 2) {
     document.getElementById('gimmed');
     gimmed.innerHTML = face;
     
     defensas.push(face + defense);
     document.getElementById("defensas").innerHTML = defensas;
   }
-  else if (d = 3) {
+  else if (d == 3) {
     document.getElementById('gimmea');
     gimmea.innerHTML = face;
     
