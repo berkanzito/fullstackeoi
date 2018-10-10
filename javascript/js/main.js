7f244b28c4f6c0fdc35a8e27693421b87adb45b5
Danilo Ulloa <danilouperez@gmail.com>
2018-10-10 16:46:34 +0100

    queryselector all

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -13,26 +13,9 @@ var outcomeText = document.getElementById('text');
 var messageDelay; //timer
 var fadeout; //timer
 var messages = [
-  'Your Bard was killed',
-  'You smote the orc',
-  'You escaped the Ice Dragon',
-  'Lightning Bolt succeeded',
-  'Critical hit',
-  'You are Lawful Evil',
-  'You fell into the Well of Sorrows',
-  'You found the Goblet of Endless Grog',
-  'You encountered a Harpy',
-  'Charisma + 10',
-  'You lose 11 Hit Points',
-  'You disarmed the trap',
-  'Plate Mail + 3',
-  '14 Damage',
-  'Spell failure',
-  'Backstab successful',
-  'Your wand broke',
-  'Surprise Attack',
-  'You broke through the door',
-  'You turned to stone'
+  'Tu Bardo fue asesinado',
+  'Carisma + 10',
+  '14 de Daño'
 ];
 
 
@@ -65,7 +48,7 @@ var showFace = function(d = 0) {
   }
 };
 
-document.querySelector('#attackDice, #defenseDice #atributeDice').addEventListener( 'click', function() {
+document.querySelectorAll('#attackDice, #defenseDice, #atributeDice').addEventListener( 'click', function() {
   //fade message
   outcome.className = '';
   //clear timers if they are there
